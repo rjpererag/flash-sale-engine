0123232b1776865ffba515924955fd614fc9323e
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2026-01-21 16:59:56 +0100

    fix/typos in env variables names

--- src/app.py
+++ src/app.py
@@ -7,8 +7,9 @@ from .redis_app.api import app
 class AppRunSettings:
     host: str = config("APP_HOST", cast=str, default="0.0.0.0")
     port: str = config("APP_PORT", cast=int, default=8080)
-    debug: str = config("APP_HOST", cast=bool, default=True)
+    debug: str = config("APP_DEBUG", cast=bool, default=True)
 
 def start_app(settings: AppRunSettings) -> None:
     settings = asdict(settings)
+    print("USING SETTINGS: ", settings)
     app.run(**settings)
--- src/redis_app/services/redis_settings.py
+++ src/redis_app/services/redis_settings.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class RedisCredentials:
     host: str = config("REDIS_HOST", cast=str, default="localhost")
-    port: int = config("REDIS_HOST", cast=int, default=6379)
-    db: int = config("REDIS_HOST", cast=int, default=0)
+    port: int = config("REDIS_PORT", cast=int, default=6379)
+    db: int = config("REDIS_DB", cast=int, default=0)
     max_connections: int = config("REDIS_MAX_CONNECTIONS", cast=int, default= 20)
     decode_responses: bool = config("REDIS_DECODE_RESPONSES", cast=bool, default=True)
