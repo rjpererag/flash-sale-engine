59288fc1c3f5034f150e3d5c42e243a54d23bf1a
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2026-01-21 13:53:19 +0100

    fix/minor fixes to status code and messages reformatting to return status code if necessary. Removing deprecated try-excepts to handle internal error from api service

--- src/redis_app/sales_app.py
+++ src/redis_app/sales_app.py
@@ -15,100 +15,63 @@ class SalesApp:
 
         self.rate_limit = rate_limit
 
-    def create_hot_product_stock(self, product_id: str, count: int) -> dict:
-        try:
-            self.inventory_manager.set_stock(
-                product_id=product_id,
-                count=count,
-            )
+    def create_hot_product_stock(self, product_id: str, count: int) -> tuple[dict, int]:
+        self.inventory_manager.set_stock(
+            product_id=product_id,
+            count=count,
+        )
 
-            return {
-                "status": 200,
-                "message": f"Stock created {product_id}:{count}"
-            }
+        return {"message": f"Stock created {product_id}:{count}"}, 200
 
-        except Exception as e:
-            return {
-                "status": 400,
-                "message": f"Error in stock creation {product_id}:{count}. {str(e)}"
-            }
 
-    def process_purchase_request(self, user_ip: str, product_id: str) -> dict:
+    def process_purchase_request(self, user_ip: str, product_id: str) -> tuple[dict, int]:
+        print(f"\n[Incoming Request from {user_ip}]")
+        allowed = self.rate_limiter.is_allowed(
+            user_id=user_ip,
+            limit=self.rate_limit
+        )
 
-        try:
-            print(f"\n[Incoming Request from {user_ip}]")
-            allowed = self.rate_limiter.is_allowed(
-                user_id=user_ip,
-                limit=self.rate_limit
-            )
+        if not allowed:
+            return {"message": "Too many requests"}, 429
 
-            if not allowed:
-                return {
-                    "status": 429,
-                    "message": "Too many requests"
-                }
+        purchase_result, buy_status = self.buy_item(product_id=product_id)
+        response, status = self.handle_purchase_result(
+            product_id=product_id,
+            result=purchase_result,
+            status=buy_status
+        )
 
-            purchase_result = self.buy_item(product_id=product_id)
-            response = self.handle_purchase_result(
-                product_id=product_id,
-                result=purchase_result
-            )
+        return response, status
 
-            return response
+    def handle_purchase_result(self, product_id: str, result: dict, status: int) -> tuple[dict, int]:
+        if (status != 200) or ("sold out" in result.get("message", str)):
+            return result, status
 
-        except Exception as e:
-            return {
-                "status": 400,
-                "message": f"Error in purchase request. {str(e)}"
-            }
-
-    def handle_purchase_result(self, product_id: str, result: dict) -> dict:
-        if result.get("status") != 200:
-            return result
-
-        if "sold out" in result.get("message", str):
-            return {
-                "status": 200,
-                "message": "Not enough stock"
-            }
-
-        leaderboard_result = self.update_leaderboard(product_id=product_id)
+        leaderboard_result, leaderboard_status = self.update_leaderboard(product_id=product_id)
         return {
-            "status": 200,
-            "message": f"{result.get('message')}. {leaderboard_result.get('message')}",
-        }
+            "message": f"{result.get('message')}. {leaderboard_result.get('message')}"
+        }, leaderboard_status
 
-    def update_leaderboard(self, product_id: str) -> dict:
+    def update_leaderboard(self, product_id: str) -> tuple[dict, int]:
         try:
             self.trending_manager.record_view(product_id=product_id)
-            return {
-                "status": 200,
-                "message": f"Leaderboard updated {product_id}"
-            }
+            return {"message": f"Leaderboard updated {product_id}"}, 200
 
         except Exception as e:
-            return {
-                "status": 400,
-                "message": f"Error in leaderboard update {product_id}. {str(e)}"
-            }
+            return {"message": f"Error in leaderboard update {product_id}. {str(e)}"}, 400
 
-    def buy_item(self, product_id: str) -> dict:
+    def buy_item(self, product_id: str) -> tuple[dict, int]:
         try:
             result = self.inventory_manager.buy_item(
                 product_id=product_id
             )
+
             msg = f"Item bought. {product_id}" if result == 1 else "sold out"
 
-            return {
-                "status": 200,
-                "message": msg
-            }
+            return {"message": msg}, 200
 
         except Exception as e:
-            return {
-                "status": 400,
-                "message": f"Error in purchase {product_id}. {str(e)}"
-            }
+            return {"message": f"Error in purchase {product_id}. {str(e)}"}, 400
 
 
 
